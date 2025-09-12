a8a15629cc655ec98e8bebcc0e195aa5e47fbe20
lunyamwis <lutherlunyamwi@gmail.com>
2025-09-12 09:54:27 +0300

    adding apache airflow providers

--- certbot-entrypoint.sh
+++ certbot-entrypoint.sh
@@ -15,15 +15,15 @@
 # certbot certonly --webroot --webroot-path=/usr/share/nginx/html --email lutherlunyamwi@gmail.org --agree-tos --no-eff-email --force-renewal -d mqtt.lunyamwi.org
 # certbot certonly --webroot --webroot-path=/usr/share/nginx/html --email lutherlunyamwi@gmail.org --agree-tos --no-eff-email --force-renewal -d demo.lunyamwi.org
 # certbot certonly --webroot --webroot-path=/usr/share/nginx/html --email lutherlunyamwi@gmail.org --agree-tos --no-eff-email --force-renewal -d airflow.lunyamwi.org
-certbot certonly \
-  --dns-cloudflare \
-  --dns-cloudflare-credentials /etc/letsencrypt/cloudflare.ini/cloudflare.ini \
-  --dns-cloudflare-propagation-seconds 60 \
-  -d lunyamwi.org \
-  -d '*.lunyamwi.org' \
-  --expand \
-  --non-interactive \
-  --agree-tos \
-  --email lutherlunyamwi@gmail.com \
-echo "done..."
+# certbot certonly \
+#   --dns-cloudflare \
+#   --dns-cloudflare-credentials /etc/letsencrypt/cloudflare.ini/cloudflare.ini \
+#   --dns-cloudflare-propagation-seconds 60 \
+#   -d lunyamwi.org \
+#   -d '*.lunyamwi.org' \
+#   --expand \
+#   --non-interactive \
+#   --agree-tos \
+#   --email lutherlunyamwi@gmail.com \
+# echo "done..."
 ## comments
